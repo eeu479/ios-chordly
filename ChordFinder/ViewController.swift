b3efa1c1805e0f1e340ba3d2148cabe8aa18ee67
Kyle Thomas <eeu479@gmail.com>
2020-04-29 13:44:41 +0100

    Initial Commit

---
+++ ChordFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChordFinder
+//
+//  Created by Kyle Thomas on 29/04/2020.
+//  Copyright © 2020 Kyle Thomas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
